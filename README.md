214983816516f78b59b93714a9036f19f30d8b34
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2022-11-13 00:29:47 -0700

    Added: import markdown notes script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # second-brain-scripts
+
 Shell scripts for managing a second brain
+
+## Usage
+
+### Import markdown notes from Android
+
+Pull all markdown files from a specific folder on your Android device. For each file we create a new file or append to existing file in the destination folder. Lastly delete the remote markdown files, but keep any other file types.
+
+```bash
+# Usage
+./import_markdown_from_android.sh <remote-folder> <destination-folder>
+```
+
+```bash
+# Example
+# Append all of my daily obsidian vault files to my logseq vault
+./import_markdown_from_android.sh /sdcard/documents/obsidian_vault /users/trey/logseq_vault/journals
+```
---
+++ import_markdown_from_android.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: ./import_markdown_from_android.sh <remote-folder> <destination-folder>"
+    exit 2
+fi
+
+# Remove any trailing slashes
+remote="${1%/}"
+dest="${2%/}"
+
+# Get the vault folder name
+vault_name="$(basename $remote)"
+
+current_timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+tmp="/tmp/import_from_android-$current_timestamp"
+mkdir -p "$tmp"
+
+# Pull files from remote
+result="$(adb pull $remote/ $tmp)"
+
+if [[ $result == *"error:"* ]]; then
+    echo $result >&2
+    exit 1
+fi
+
+# If the pull result was successful
+if [[ $result == *"0 skipped"* ]]; then
+    num_files="$(find $tmp -name '*.md' | wc -l)"
+    
+    if [ $num_files -eq 0 ]; then
+        echo "0 markdown files were pulled from remote."
+        exit 1
+    fi
+    
+    for file in $tmp/$vault_name/*.md; do
+        filename="$(basename $file)"
+        file_data=`cat $file`
+        
+        output_file="$dest/$filename"
+        
+        # If file exists, append data to it, otherwise write to a new file
+        if [ -f "$output_file" ]; then
+            echo "Updating: $filename"
+            echo "$file_data" >> "$output_file"
+        else
+            echo "Writing: $filename"
+            echo "$file_data" > "$output_file"
+        fi
+    done
+    
+    # Delete remote files
+    adb shell rm -r $remote/*.md
+fi
